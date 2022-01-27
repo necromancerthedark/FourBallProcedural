a9ebf679d69f90376e8fa0c5c1981a4abcd2ddd7
Kumar Gaurav Pandey <kg22584@gmail.com>
2022-01-27 11:31:28 +0530

    cleaned code a bit

--- src/fourBall.java
+++ src/fourBall.java
@@ -5,6 +5,7 @@ public class fourBall extends PApplet {
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
     public static final int bgcolor = 255;
+    public static final int distanceDivisor = 6;
     int x1 =0;
     int x2 =0;
     int x3 =0;
@@ -27,35 +28,25 @@ public class fourBall extends PApplet {
 
     @Override
     public void draw() {
-        getBackground();
+//        getBackground();
         int radius = 10;
-        getEllipse_1(radius,1,1);
-        getEllipse_2(radius,2,2);
-        getEllipse_3(radius,3,3);
-        getEllipse_4(radius,4,4);
-
+        getEllipse(radius,1,x1);
+        getEllipse(radius,2,x2);
+        getEllipse(radius,3,x3);
+        getEllipse(radius,4,x4);
+        changeDistance();
     }
 
-    private void getEllipse_1(int radius, int distance, int speed) {
-        ellipse(0 + x1,(HEIGHT*distance)/6, radius, radius);
-        x1+=speed;
+    private void getEllipse(int radius, int distance, int displacementVector) {
+        ellipse(0 + displacementVector,(HEIGHT*distance)/ distanceDivisor, radius, radius);
     }
 
-    private void getEllipse_2(int radius, int distance, int speed) {
-        ellipse(0 + x2,(HEIGHT*distance)/6, radius, radius);
-        x2+=speed;
+    private void changeDistance(){
+        x1+=1;
+        x2+=2;
+        x3+=3;
+        x4+=4;
     }
-
-    private void getEllipse_3(int radius, int distance, int speed) {
-        ellipse(0 + x3,(HEIGHT*distance)/6, radius, radius);
-        x3+=speed;
-    }
-
-    private void getEllipse_4(int radius, int distance, int speed) {
-        ellipse(0 + x4,(HEIGHT*distance)/6, radius, radius);
-        x4+=speed;
-    }
-
 
     private void getBackground() {
         background(bgcolor);
